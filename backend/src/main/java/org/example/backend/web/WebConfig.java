12acedafdce4d01615e23d5b5f50e6b7509d70a8
Junaid <junaiduthman123@gmail.com>
2025-10-08 02:47:25 +0200

    configured corse

--- backend/src/main/java/org/example/backend/web/WebConfig.java
+++ backend/src/main/java/org/example/backend/web/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")  // your frontend URL
+                        .allowedOrigins("https://easyjob-six-zeta.vercel.app/") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
